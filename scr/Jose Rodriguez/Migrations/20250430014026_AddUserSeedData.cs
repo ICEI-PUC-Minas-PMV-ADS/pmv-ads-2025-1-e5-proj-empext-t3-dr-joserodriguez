69a31019eb2bc362e10318eccd84b009492e9d19
biancametier <biamoroll.me@gmail.com>
2025-04-29 22:44:02 -0300

    Alterar login padrão

---
+++ scr/Jose Rodriguez/Migrations/20250430014026_AddUserSeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jose_Rodriguez.Migrations
+{
+    public partial class AddUserSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password" },
+                values: new object[] { 1, "Consultoriodontovip@gmail.com", "odontovipJR294" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
--- scr/Jose Rodriguez/Models/ApplicationDbContext.cs
+++ scr/Jose Rodriguez/Models/ApplicationDbContext.cs
@@ -15,5 +15,19 @@ namespace SeuProjeto.Models // Altere para o namespace correto
         public DbSet<Patient> Patients { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Email = "Consultoriodontovip@gmail.com",
+                    Password = "odontovipJR294"
+                }
+            );
+        }
     }
 }
