131e2162e7c72fa51478f2aa44dfd6930fe54a78
Geovanne do Monte <geovannedomonte@gmail.com>
2025-05-28 08:20:32 -0300

    update Dentista.cs

--- scr/Jose Rodriguez/Models/Dentista.cs
+++ scr/Jose Rodriguez/Models/Dentista.cs
@@ -1,23 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SeuProjeto.Models // Altere para o namespace correto
+namespace LoginCadastroMVC.Models
 {
     public class Dentista
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string Nome { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome deve ter até 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
 
-        [Required]
-        [Display(Name = "Cédula Profissional")]
-        public string Cedula { get; set; }
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        [StringLength(100, ErrorMessage = "O email deve ter até 100 caracteres")]
+        public string Email { get; set; } = string.Empty;
 
-        [Phone]
-        public string Telefone { get; set; }
+        [StringLength(20, ErrorMessage = "O telefone deve ter até 20 caracteres")]
+        public string Telefone { get; set; } = string.Empty;
 
-        [EmailAddress]
-        public string Email { get; set; }
+        [StringLength(50, ErrorMessage = "A célula deve ter até 50 caracteres")]
+        public string Cedula { get; set; } = string.Empty;
+
+        [StringLength(20, ErrorMessage = "O CRO deve ter até 20 caracteres")]
+        public string? CRO { get; set; }
+
+        [StringLength(100, ErrorMessage = "A especialidade deve ter até 100 caracteres")]
+        public string? Especialidade { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
     }
 }
